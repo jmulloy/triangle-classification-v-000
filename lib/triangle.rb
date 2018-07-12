cbdcab0467468d823077bbf43d3f46650497930d
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-12 18:14:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,6 +2,8 @@ class Triangle
   attr_accessor :equilateral, :isosceles, :scalene
 
   def initialize(side_1,side_2,side_3)
-    @side_1 = 
+    @side_1 = 2
+    @side_2 = 2
+    @side_3 = 3
   end
 end

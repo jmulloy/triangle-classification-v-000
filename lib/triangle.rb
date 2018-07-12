cd23c6d0b0d2fd85cb26933d07d89bfa7beb371e
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-12 19:41:34 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -25,7 +25,6 @@ class Triangle
   end
 
   def validation_method
-    #binding.pry
     (@side_1 > 0 && @side_2 > 0 && @side_3 > 0) && (@side_1 + @side_2 > @side_3 && @side_3 + @side_2 > @side_1 && @side_3 + @side_1 > @side_2)
   end
 

1d3a2c9c50e9a4f46c5c1cbd1567ac66b7a12463
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-12 19:27:13 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -33,12 +33,12 @@ class Triangle
     (@side_1 > 0 && @side_2 > 0 && @side_3 > 0) && (@side_1 + @side_2 >= @side_3 || @side_3 + @side_2 >= @side_1 || @side_3 + @side_1 >= @side_2)
 
   end
-end
 
-class TriangleError < StandardError
-  def message
-    "Your triangle is not valid"
+
+  class TriangleError < StandardError
+    def message
+      "Your triangle is not valid"
+    end
   end
-
 
 end
